f72afa3bd486ef0598c3ebf40b44713be31fb0fd
Timothy Emeigh <tre8@psu.edu>
2026-09-02 16:21:44 -0400

    Adding documentation.

--- science_analysis/bc_image_analyzer.py
+++ science_analysis/bc_image_analyzer.py
@@ -1,3 +1,5 @@
+"""Module for running image-related analysis of BlackCAT eventlists."""
+
 from collections.abc import Collection, Iterable
 from os import PathLike
 from pathlib import Path
@@ -49,6 +51,32 @@ class BCImageAnalysis:
         hide_frame: bool = True,
         overwrite: bool = False,
     ):
+        """Class for image-related analysis of BlackCAT eventlists.
+
+        Arguments:
+            - caldb_version: (Optional) BlackCAT CalDB version string.
+            Mutually exclusive with coded_mask_file and teldef_file.
+            - coded_mask_file: (Optional) Path to BlackCAT Aperture
+            CalDB file. Mutually exclusive with caldb_version.
+            - teldef_file: (Optional) Path to BlackCAT Teldef CalDB
+            file. Mutually exclusive with caldb_version.
+            - ra_dec_roll_inst: (Optional) Initial ra, dec, and roll
+            for eventlist pointing. Defaults to [0, 0, 0] if not
+            provided.
+            - use_subpixel: Whether or not to use subpixels for
+            imaging. Very memory heavy.
+            - resolution: Imaging resolution (detector [sub]pixel size
+            projected by focal length). 1=Finest (detector [sub]pixel
+            size). 8 or 24 = Coarsest (mask cell size. 8 if not
+            use_subpixel, 24 if use_subpixel.)
+            - balance_per_det: Whether to balance each of the four
+            detector DPHs individually. Uses provided array of
+            bounding boxes, or generates based on CalDB if 'True'.
+            - hide_frame: Sets the support structure opacity to 50% to
+            compensate for edge shadow pattern.
+            - overwrite: Any files generated will overwrite existing
+            files with shared names, instead of raising an exception.
+        """
         if ra_dec_roll_inst is None:
             ra_dec_roll_inst = [0, 0, 0]
         if len(ra_dec_roll_inst) != 3:
@@ -72,14 +100,21 @@ class BCImageAnalysis:
 
     @property
     def imager(self) -> BCImager:
+        """Imager object in use by current instance of
+        BCImageAnalysis.
+        """
         return self._imager
 
     @property
     def overwrite(self) -> bool:
+        """Whether current instance of BCImageAnalysis will overwrite
+        files.
+        """
         return self._overwrite
 
     @property
     def ra_dec_roll(self) -> list[float]:
+        """Current RA, DEC, and roll for eventlist pointing."""
         return self._ra_dec_roll
 
     @overload
@@ -108,6 +143,26 @@ class BCImageAnalysis:
         add_wcs: bool = False,
         outfile: Optional[PathLike | str] = None,
     ) -> fits.PrimaryHDU:
+        """Create a sky image from provided event data. Event
+        positions should be provided in DET coordinates. Nominally,
+        (DETX_axis, DETY_axis) are aligned with (-SATZ_axis,
+        -SATY_axis).
+
+        Returns a FITs HDU. Writes that HDU to a fits file if a path
+        is provided.
+
+        Arguments:
+            - counts: (Optional) Structured array containing data for
+            each event. Mututally exclusive with fitsdata.
+            - header: (Optional) FITs header for the events provided
+            by counts. Unused if fitsdata provided.
+            - fitsdata: (Optional) FITs BinTableHDU or path to a fits
+            file containing a BinTableHDU of events. Mutually
+            exclusive with counts.
+            - add_wcs: Whether to use current RA, DEC, and roll to
+            add WCS keywords to the image header.
+            - outfile: (Optional) Path to write the generate image to.
+        """
         counts, header = self._counts_header_from_args(
             counts=counts, header=header, fitsdata=fitsdata
         )
@@ -124,11 +179,20 @@ class BCImageAnalysis:
         return image_hdu
 
     def get_exposed_area_map(
-        self, active_det_ids: Iterable[int]
+        self, active_det_ids: npt.ArrayLike[np.integer[Any]]
     ) -> npt.NDArray[np.float64]:
+        """Get a map of exposed focal plane area for each sky pixel,
+        provided which of the four detectors are active. Values are in
+        m^2.
+
+        Arguments:
+            active_det_ids: ArrayLike object of up to four unique
+            detector IDs indicating that detector is to be considered
+            active.
+        """
         exposed_area_map = np.zeros(self.imager.image_minshape, dtype=np.float64)
 
-        for det_id in active_det_ids:
+        for det_id in np.unique(active_det_ids):
             exposed_area_map += self.imager.exposed_area_maps[det_id]
 
         return exposed_area_map
@@ -143,6 +207,7 @@ class BCImageAnalysis:
         dec: Optional[float] = None,
         roll: Optional[float] = None,
     ) -> None:
+        """Change the RA, DEC, and roll for eventlist pointing."""
         if dec is None and roll is None:
             if len(ra) == 3:
                 ra, dec, roll = ra
@@ -154,6 +219,8 @@ class BCImageAnalysis:
         self._ra_dec_roll = [ra, dec, roll]
 
     def _add_wcs_keywords(self, hdu: fits.PrimaryHDU) -> fits.PrimaryHDU:
+        # Calculates and adds WCS keywords to the provided HDU
+
         # WCS keywords refer to the center of the pixel, but 1-idx origin
         # So for a 1x1 image, the center of the (only) pixel is (1., 1.)
         pixd1 = (hdu.data.shape[1] + 1) / 2.0
@@ -223,6 +290,8 @@ class BCImageAnalysis:
         header: Optional[fits.Header] = None,
         fitsdata: Optional[fits.BinTableHDU | PathLike | str] = None,
     ) -> tuple[npt.NDArray[np.void], fits.Header]:
+        # Reads the typical arguments for provided eventlists and
+        # returns the counts and header.
         if (counts is not None) == (fitsdata is not None):
             raise TypeError(
                 "You must specify exactly one of a counts array or FITS data."
